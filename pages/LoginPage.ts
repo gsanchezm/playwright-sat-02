f5d1b5b7e27f35c5e100190f9afd29cd2830dd86
gsanchezm <gilsanchezts@outlook.com>
2026-07-25 12:21:14 -0600

    feature: added POM

---
+++ pages/LoginPage.ts
@@ -0,0 +1,63 @@
+import { expect, type Locator } from "@playwright/test";
+import { BasePage } from "./BasePage";
+import type { CountryCode, User } from "../types";
+
+export class LoginPage extends BasePage{
+    readonly path = "/";
+
+    // Object Repository
+    private txtUsername: string = "username";
+    private txtPassword: string = "password";
+    private btnMarket: string = "market-";
+    private btnSignIn: string = "login-button";
+    private lblError: string = "login-error";
+
+    // Accessor y Mutator - Getters y Setter
+    private get usernameInput():Locator{
+        return this.tid(this.txtUsername);
+    }
+
+    private get passwordInput():Locator{
+        return this.tid(this.txtPassword);
+    }
+
+    private get signInButton():Locator{
+        return this.tid(this.btnSignIn);
+    }
+
+    private get errorMessage(): Locator{
+        return this.tid(this.lblError);
+    }
+
+    private marketFlag(contryCode: CountryCode): Locator{
+        return this.tid(`${this.btnMarket}${contryCode}`)
+    }
+
+     async goTo(): Promise<void>{
+        await this.page.goto(this.path)
+     }
+
+     /* async typeUsername(username: string): Promise<void>{
+        this.usernameInput.fill(username);
+     } */
+
+    async selectMarket(code: CountryCode): Promise<void>{
+        await this.marketFlag(code).click();
+    }
+
+    async loginAs(user: User): Promise<void>{
+        await this.usernameInput.fill(user.username);
+        await this.passwordInput.fill(user.password);
+        await this.signInButton.click();
+    }
+
+    async loginMarket(user: User, code: CountryCode): Promise<void>{
+        await this.selectMarket(code);
+        await this.loginAs(user);
+        await this.waitForUrl(/\/catalog/);
+    }
+
+    async verifyLoginError(): Promise<void>{
+        await expect(this.errorMessage).toBeVisible();
+    }
+}
