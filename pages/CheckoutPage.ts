eecb519404b55b6a7d8a0fd0271025badb97ac8a
drodela <denisserodela83@gmail.com>
2026-07-28 16:27:18 -0700

    Denisse update CheckoutPage 1

--- pages/CheckoutPage.ts
+++ pages/CheckoutPage.ts
@@ -2,3 +2,11 @@ import { expect, type Locator } from "@playwright/test";
 import { BasePage } from "./BasePage";
 
 export class CheckoutPage extends BasePage{}
+readonly path = "/checkout";
+
+  private txtFullName: string = "full-name";
+  private txtPhone: string = "phone";
+  private txtAddress: string = "address";
+
+  // Error a proposito que pasa cuando te equivocas o te dan observaciones 
+  // a corregir?
