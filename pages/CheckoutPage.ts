952d441e8634789338c25f36220365483c326d61
drodela <denisserodela83@gmail.com>
2026-07-29 12:17:40 -0700

    fix: ya corregi la clase CheckoutPage

--- pages/CheckoutPage.ts
+++ pages/CheckoutPage.ts
@@ -1,12 +1,13 @@
 import { expect, type Locator } from "@playwright/test";
 import { BasePage } from "./BasePage";
 
-export class CheckoutPage extends BasePage{}
-readonly path = "/checkout";
-
+export class CheckoutPage extends BasePage{
+  readonly path = "/checkout";
   private txtFullName: string = "full-name";
   private txtPhone: string = "phone";
   private txtAddress: string = "address";
+}
+
 
   // Error a proposito que pasa cuando te equivocas o te dan observaciones 
-  // a corregir?
+  // a corregir? YA SE CORRIGO
