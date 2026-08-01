d7bed705e4a28c71af54c65b1010f2ab7c29e4c3
CarolinaSalas180188 <carolinasalas180188@gmail.com>
2026-07-31 21:28:44 -0700

    Code indentation was adjusted for better readability

--- pages/ProfilePage.ts
+++ pages/ProfilePage.ts
@@ -1,10 +1,8 @@
 import { expect, type Locator } from "@playwright/test";
 import { BasePage } from "./BasePage";
 
-export class ProfilePage extends BasePage{
-readonly path = "/profile";
-
+export class ProfilePage extends BasePage {
+  readonly path = "/profile";
   private txtFullName: string = "profile-fullname";
   private txtPhone: string = "profile-phone";
-
 }
